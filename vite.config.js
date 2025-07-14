8903580efeb84c85d04af372f045355aaf1b27a2
Abhishek Singh <rathoreabhishek006@gmail.com>
2025-07-15 02:14:58 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/MERN-Stack-Project/", // <- your GitHub repo name with slashes
   plugins: [react()],
-})
+});
